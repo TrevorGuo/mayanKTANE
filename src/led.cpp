aa6a703d68c73a0524086f96a519fe486685c335
domkolson <34842354+domkolson@users.noreply.github.com>
2023-01-21 04:15:08 -0800

    led class

---
+++ src/led.cpp
@@ -0,0 +1,17 @@
+#include "led.h"
+#include "Arduino.h"
+led::led(int pin) {
+    m_pin = pin;
+    pinMode(m_pin, OUTPUT);
+    digitalWrite(m_pin, LOW);
+}
+
+// turn the LED on.
+void led::on() {
+    digitalWrite(m_pin, HIGH);
+}
+
+// turn the LED off.
+void led::off() {
+    digitalWrite(m_pin, LOW);
+}
