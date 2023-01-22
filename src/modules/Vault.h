a3b888b5035ed85a050d71181f84c8cf5081bef7
Gautier404 <and.jgautier@gmail.com>
2023-01-22 01:02:28 -0800

    merged in main

--- src/modules/Vault.h
+++ src/modules/Vault.h
@@ -2,14 +2,40 @@
 #define VAULT_H
 
 #include <stdlib.h>
+#include <Arduino.h>
 
-#define CODE_LENGTH 4
+#define POTPIN 4
+#define CODE_LENGTH 3
+#define ADCALPHAFILTER 0.3
+#define ADC_RESOLUTION 4032
+#define NUM_NUMERALS 8
+#define WAIT_TIME_SEC 10
+
+const int regionToNumeralMap[NUM_NUMERALS] =  {1,2,3,4,5,6,7,8};
+
+
+class Bomb;
 
 class Vault {
     public:
         Vault();
+        void runVault();
     private:
-        int values[CODE_LENGTH]; 
+        void initializeCombo();
+        void comboSuccess();
+        void comboFail();
+        void calcFinalNumeral();
+        void StartBuzz();
+        void StopBuzz();
+
+        int m_regions[CODE_LENGTH];
+        int m_ordered_regions[CODE_LENGTH];
+        int m_final_numeral;
+        int m_potentiometerValue;
+        int m_region;
+        bool clockActive;
+        long clockStart;
 };
+
 
 #endif
