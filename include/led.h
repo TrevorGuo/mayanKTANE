480de411c9fd575503b295f9bab0d2b8010ef89b
TrevorGuo <trevorguo@gmail.com>
2023-01-22 06:02:20 -0800

    Constants update

--- include/led.h
+++ include/led.h
@@ -2,18 +2,18 @@
 #define LED_H
 
 // may need to reallocate these.
-#define LED_0 13
-#define LED_1 12
-#define LED_2 14
-#define LED_3 27
-#define LED_4 26
-#define LED_5 25
-#define LED_6 33
-#define LED_7 32
-#define LED_8 35
-#define LED_9 34
-#define LED_10 2
-#define LED_11 4
+// #define LED_0 13
+// #define LED_1 12
+// #define LED_2 14
+// #define LED_3 27
+// #define LED_4 26
+// #define LED_5 25
+// #define LED_6 33
+// #define LED_7 32
+// #define LED_8 35
+// #define LED_9 34
+// #define LED_10 2
+// #define LED_11 4
 
 #define NUM_LEDS 12
 
--- include/utils.h
+++ include/utils.h
@@ -5,21 +5,45 @@
 
 //Constants
 #define NUM_MODULES 3
+#define DEBOUNCER_TIME 100
 
 //Pin Mappings
-#define BUTTON_0 34
-#define BUTTON_1 36
-#define BUTTON_2 32
-#define BUTTON_3 35
-#define BUTTON_4 33
-#define BUTTON_5 39
-
+#define BUTTON_0 4  //Brown D4
+#define BUTTON_1 33 //Orange D33
+#define BUTTON_2 5 // Grey D5
+#define BUTTON_3 16 // Black 
+#define BUTTON_4 17 // White D18
+#define BUTTON_5 32 //Red D32
 #define LED_0 25
 #define LED_1 26
 #define LED_2 14
 #define LED_3 13
 #define LED_4 12
 #define LED_5 27
+
+#define ACC_SCL 22
+#define ACC_SDA 21
+
+#define SERVO 23
+#define SERVO_BUTTON 1
+
+#define NEOPIXEL_PIN 18
+
+// #define BUTTON_0 27
+// #define BUTTON_1 12
+// #define BUTTON_2 13
+// #define BUTTON_3 14
+// #define BUTTON_4 26
+// #define BUTTON_5 25
+
+// #define LED_0 39
+// #define LED_1 33
+// #define LED_2 35
+// #define LED_3 32
+// #define LED_4 36
+// #define LED_5 34
+
+
 
 //Generates a random number between a and b
 int randInt(int a, int b);
